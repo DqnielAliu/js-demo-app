363be6127415dad0044abe35e4c623b9c97e52a1
Daniel Aliu <contact.danielaliu@gmail.com>
2021-10-01 17:02:11 +0100

    aikaf  msg

--- script.groovy
+++ script.groovy
@@ -1,6 +1,7 @@
 
 def buildApp() {
-    echo 'building the application...' 
+    echo 'building the application...'
+    echo ' checking periodic trigger' 
     }
 def testApp() {
      echo 'testing the application...'
